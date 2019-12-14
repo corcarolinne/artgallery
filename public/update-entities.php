c2c2f13e9d68203bd6e96bfa75ec43cfc6e8cabe
Carolinne Cordeiro <carolinnemcordeiro@gmail.com>
2019-12-14 15:11:29 +0000

    allowing administrators to update other administrators's username and email

--- public/update-entities.php
+++ public/update-entities.php
@@ -43,19 +43,41 @@ if(isset($_GET['adminToBeEdited'])){
         // Escape user inputs for security
         $first_name = mysqli_real_escape_string($connection, $_POST['first_name']);
         $last_name = mysqli_real_escape_string($connection, $_POST['last_name']);
+        $username = mysqli_real_escape_string($connection, $_POST['username']);
+        $email = mysqli_real_escape_string($connection, $_POST['email']);
         $password = mysqli_real_escape_string($connection, $_POST['password']);
         $address = mysqli_real_escape_string($connection, $_POST['address']);
     
+        $sql_checkUser = "SELECT * FROM users WHERE Username='$username'";
+        $sql_checkEmail = "SELECT * FROM users WHERE Email='$email'";
+        $res_u = mysqli_query($connection, $sql_checkUser);
+        $res_e = mysqli_query($connection, $sql_checkEmail);
+
+        // saving UserID corresponding to the result of the query of usernames
+        $row_u= mysqli_fetch_array($res_u);
+        $userID_from_username_check = $row_u['UserID'];
+
+        // saving UserID corresponding to the result of the query of emails
+        $row_e= mysqli_fetch_array($res_e);
+        $userID_from_email_check = $row_e['UserID'];
+
         // first, check if the required fields are not empty
-        if (empty($first_name) || empty($last_name) || empty($password)) {
+        if (empty($first_name) || empty($last_name) || empty($email) || empty($username) || empty($password)) {
             $empty_field_error = "This field is required"; 		
         }else{
-            // Attempt insert query
-            $sql = "UPDATE users
-                SET FirstName= '$first_name', LastName= '$last_name', Pass= '$password', Address= '$address'
-                WHERE UserID = '$adminToBeEdited';";
-            $results = mysqli_query($connection, $sql);
-            header('Location: admin-dashboard.php');
+            // if it's not empty, checking if there's not any other user with the same username or email
+            if (mysqli_num_rows($res_u) > 0 && $userID_from_username_check !== $adminToBeEdited) {
+                $name_error = "Sorry, this username was already taken"; 	
+            }else if(mysqli_num_rows($res_e) > 0 && $userID_from_email_check !== $adminToBeEdited) {
+                $email_error = "Sorry, this email was already used"; 	
+            }else{
+                // Attempt update query
+                $sql = "UPDATE users
+                        SET FirstName= '$first_name', LastName= '$last_name', Username= '$username', Pass= '$password', Address= '$address', Email= '$email'
+                        WHERE UserID = '$adminToBeEdited';";
+                $results = mysqli_query($connection, $sql);
+                header('Location: admin-dashboard.php');
+            }
         }
     }   
 }
